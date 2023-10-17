9f2d02b71742b403c4722c348492c2d11043396e
Dhyan <kdhyan94@gmail.com>
2023-10-18 00:18:28 +0530

    TICTACTOE game added

---
+++ TicTacToe.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        initializeBoard();
+        boolean gameOver = false;
+
+        while (!gameOver) {
+            printBoard();
+            makeMove();
+            gameOver = checkGameOver();
+            switchPlayer();
+        }
+    }
+
+    private static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    private static void printBoard() {
+        System.out.println("  1 2 3");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" " + board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void makeMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        do {
+            System.out.println("Player " + currentPlayer + ", enter row (1-3) and column (1-3) to place your symbol:");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+        } while (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ');
+
+        board[row][col] = currentPlayer;
+    }
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private static boolean checkGameOver() {
+        if (checkWin(currentPlayer)) {
+            printBoard();
+            System.out.println("Player " + currentPlayer + " wins! Congratulations!");
+            return true;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+
+        printBoard();
+        System.out.println("It's a draw!");
+        return true;
+    }
+
+    private static boolean checkWin(char player) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true; // Check rows
+            }
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                return true; // Check columns
+            }
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true; // Check diagonal (top-left to bottom-right)
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true; // Check diagonal (top-right to bottom-left)
+        }
+        return false;
+    }
+}
